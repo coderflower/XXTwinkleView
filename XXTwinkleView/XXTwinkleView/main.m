f7fd98b042c8ede6202291058f7d730770e1ebdc
longjun <longjuncai@meifabao.cn>
2016-10-10 21:20:53 +0800

    初始化项目

---
+++ XXTwinkleView/XXTwinkleView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  XXTwinkleView
+//
+//  Created by mac on 2016/10/10.
+//  Copyright © 2016年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
